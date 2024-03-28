e3a6f139c2cf07b11aaa89ff78d7083c4e56a394
AntonBabychP1T <antonbabi13@gmail.com>
2024-03-28 18:44:19 +0200

    Initial commit

---
+++ src/main/java/education/kafkapratice/KafkaPraticeApplication.java
@@ -0,0 +1,13 @@
+package education.kafkapratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/education/kafkapratice/KafkaPraticeApplicationTests.java
@@ -0,0 +1,8 @@
+package education.kafkapratice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPraticeApplicationTests {
+
+}
